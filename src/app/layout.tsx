b36d44b7467a26fdf9817cb75932e761ef20dda5
hi-rachel <rachel.uiux@gmail.com>
2025-07-25 17:03:26 +0900

    feat: open graph 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,29 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Type Trip",
-  description: "당신의 여행 성향을 찾아주는 캐릭터 기반 테스트",
+  description:
+    "여행, 나다운 방식으로! 캐릭터로 알아보는 나만의 여행 성향 테스트",
+  openGraph: {
+    title: "Type Trip",
+    description:
+      "여행, 나다운 방식으로! 캐릭터로 알아보는 나만의 여행 성향 테스트",
+    url: "https://type-trip.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "/images/og.png",
+        width: 1200,
+        height: 630,
+        alt: "Type Trip 오픈그래프 이미지",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Type Trip",
+    description: "당신의 여행 성향을 찾아주는 캐릭터 기반 테스트",
+    images: ["/images/og.png"],
+  },
 };
 
 const RootLayout = ({
